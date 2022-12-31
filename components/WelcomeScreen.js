50406341db30c1f4a98cb6a39a60b55b2b9bdb60
yaqubadesola <Admin@@2018>
2023-01-01 00:50:58 +0100

    Image Component

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { Image, View, Text, ScrollView, StyleSheet } from 'react-native'
 const  WelcomeScreen = () => {
   return (
     <ScrollView style={{flex: 1}}
     indicatorStyle={"white"}
     > 
+        <View style={welComeScreenStyles.headerWrapper}>
+            <Image 
+              source={require('../img/logo.png')}
+              resizeMode="contain"
+              style={welComeScreenStyles.logo}
+              accessible={true}
+              accessibilityLabel={'Little Lemon Logo'}
+
+              />
+              <Text style={welComeScreenStyles.headerText}>
+                  Little Lemon
+              </Text>
+        </View>
         <Text 
-           style={welComeScreenStyles.welcomeHeader}
+          style={welComeScreenStyles.welcomeHeader}
         >
-              Welcome to Little Lemon
+              Welcome to Little Lemon App
         </Text>
         <Text 
             style={welComeScreenStyles.welcomeText}>
@@ -20,12 +33,32 @@ const  WelcomeScreen = () => {
 }
 
 const welComeScreenStyles = StyleSheet.create({
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  headerText: {
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
   welcomeHeader:{
       padding: 40,
       fontSize: 30,
-      color: '#880000',
+      color: '#88fff6',
       fontWeight:"bold",
       textAlign: 'center',
+  },
+  logo:{
+    height:100,
+    width:100,
+    borderRadius:20,
+    color: '#88fff6',
   },
   welcomeText:{
     fontSize: 24,
