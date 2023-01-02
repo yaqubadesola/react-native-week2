1f345b6c36282c0ab3c5c2f4f5090b77b2d2c2fb
yaqubadesola <Admin@@2018>
2023-01-02 11:16:44 +0100

    Bottom Tab Navigation

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import LittleLemonFooter from './components/LittleLemonFooter';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MenuItemList from './components/MenuItemList';
 import MenuItems from './components/MenuItems';
 import WelcomeScreen from './components/WelcomeScreen';
+import { Ionicons } from '@expo/vector-icons';
 export default function App() {
   const Stack = createNativeStackNavigator()
+  const Tab = createBottomTabNavigator()
   
   const LogoTitle = () => {
     return <Image 
@@ -30,8 +33,32 @@ export default function App() {
         <MenuItemList/>
        <FeedbackFormWithProps/> 
         <FeedbackForm/> */}
-     
-      <Stack.Navigator 
+          <Tab.Navigator
+              screenOptions={({route}) => ({
+                tabBarIcon :({focused, color, size}) => {
+                  let iconName
+
+                  if(route.name === "Welcome"){
+                    iconName = focused? "ios-information-circle" : "ios-information-circle-outline"
+                  } else if(route.name === "Menu"){
+                    iconName = "ios-list"
+                  }else if(route.name === "Login"){
+                    iconName = "ios-enter"
+                  }
+                 
+                  return <Ionicons name={iconName} size={size} color={color}/>;
+
+                },
+                tabBarActiveTintColor: '#333333',
+                tabBarInactiveTintColor: 'gray',
+              })}
+             
+          > 
+            <Tab.Screen name="Login" component={FeedbackFormWithProps}/>
+            <Tab.Screen name="Welcome" component={WelcomeScreen} />
+            <Tab.Screen name="Menu" component={MenuItemList} />
+          </Tab.Navigator>
+      {/* <Stack.Navigator 
           initialRouteName="Welcome" 
           screenOptions={{ 
             headerStyle: { backgroundColor: '#333333', justifyContent:"center"},
@@ -49,7 +76,7 @@ export default function App() {
           />
           <Stack.Screen name="Login" component={FeedbackFormWithProps}/>
           <Stack.Screen name="Menu" component={MenuItemList}/>
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </View>
     <View
       style={{
